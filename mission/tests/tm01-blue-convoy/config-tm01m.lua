df30ed3eb28ba43198f439503a258d3a86837e90
birkenmoped <166322503+birkenmoped@users.noreply.github.com>
2026-07-26 15:53:13 +0200

    Configure TM01M for Mission Editor MSR pathlines

--- mission/tests/tm01-blue-convoy/config-tm01m.lua
+++ mission/tests/tm01-blue-convoy/config-tm01m.lua
@@ -1,11 +1,11 @@
 local config = {
-  configurationVersion = "TM01M-moose-native-physical-3",
+  configurationVersion = "TM01M-moose-native-msr-pathline-1",
   testId = "TM01",
   stageId = "TM01M",
 
   mission = {
     fileName = "OMW_TEST_TM01M_MooseFirst.miz",
-    displayName = "OMW TM01M - MOOSE Native Physical Convoy",
+    displayName = "OMW TM01M - MOOSE Native MSR Convoy",
   },
 
   template = {
@@ -17,21 +17,28 @@ local config = {
   zones = {
     start = "ZONE_TM01_START_BAGRAM",
     target = "ZONE_TM01_TARGET_JALALABAD",
-    routeAnchors = {
-      "ZONE_TM01_ROUTE_01",
-      "ZONE_TM01_ROUTE_02",
-      "ZONE_TM01_ROUTE_03",
-      "ZONE_TM01_ROUTE_04",
-      "ZONE_TM01_ROUTE_05",
-      "ZONE_TM01_ROUTE_06",
-      "ZONE_TM01_ROUTE_07",
-    },
   },
 
   routing = {
+    msrPathlines = {
+      "MSR_EAST_E03",
+      "MSR_EAST_E02",
+    },
     speedKph = 30,
-    offRoadFormation = "Off Road",
+    formation = "On Road",
     routeDelaySeconds = 1,
+    routeStartLeadMeters = 100,
+    waypointSpacingMeters = 2500,
+    vehicleSpacingMeters = 18,
+    minimumVehicleSeparationMeters = 8,
+    spawnRearClearanceMeters = 20,
+    headingSampleMeters = 10,
+    minimumRoutePointSeparationMeters = 2,
+    minimumWaypointSeparationMeters = 25,
+    maximumZoneRoadSnapMeters = 175,
+    maximumSpawnRoadSnapMeters = 30,
+    maximumPathlineJoinMeters = 250,
+    maximumWaypointRoadSnapMeters = 250,
   },
 
   supervision = {
