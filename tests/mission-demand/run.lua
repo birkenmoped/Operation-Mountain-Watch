ce7c80ba6980b41b7f7105289abd8f26ab1c7024
birkenmoped <166322503+birkenmoped@users.noreply.github.com>
2026-08-29 22:32:30 +0200

    Run Stage 2B CAS dispatch tests

--- tests/mission-demand/run.lua
+++ tests/mission-demand/run.lua
@@ -2,5 +2,6 @@ dofile("tests/mission-demand/test_mission_demand.lua")
 dofile("tests/mission-demand/test_resource_demand_policy.lua")
 dofile("tests/mission-demand/test_fob_attack_demand_policy.lua")
 dofile("tests/mission-demand/test_fob_threat_opszone_adapter.lua")
+dofile("tests/mission-demand/test_fob_attack_cas_dispatch_adapter.lua")
 
 print("PASS mission-demand test suite")
