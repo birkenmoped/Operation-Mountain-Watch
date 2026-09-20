4f9dec85ff728fa7ec82021ec62c97815dd632d2
birkenmoped <166322503+birkenmoped@users.noreply.github.com>
2026-09-20 14:18:14 +0200

    test: run shared CAS lifecycle contract

--- tests/mission-demand/run.lua
+++ tests/mission-demand/run.lua
@@ -34,6 +34,7 @@ dofile("tests/mission-demand/test_fire_support_strategic_resupply_resupply_monit
 dofile("tests/mission-demand/test_fire_support_strategic_resupply_commander_bridge.lua")
 dofile("tests/mission-demand/test_fire_support_strategic_resupply_arty_mission_factory.lua")
 dofile("tests/mission-demand/test_fire_support_strategic_resupply_cas_mission_factory.lua")
+dofile("tests/mission-demand/test_fire_support_strategic_resupply_cas_lifecycle_runtime.lua")
 dofile("tests/mission-demand/test_fire_support_strategic_resupply_external_support_runtime.lua")
 dofile("tests/mission-demand/test_fire_support_strategic_resupply_storage_transport_factory.lua")
 dofile("tests/mission-demand/test_fire_support_strategic_resupply_transport_runtime.lua")
